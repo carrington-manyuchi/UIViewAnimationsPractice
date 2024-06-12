8f3a779b2094f67dc0a6e292b34a09118993f207
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-12 15:46:41 +0200

    basic animation

--- UIViewAnimationsPractice/ViewController.swift
+++ UIViewAnimationsPractice/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var block: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            self.upMove()
+        }
     }
 
-
+    func upMove() {
+        UIView.animate(withDuration: 1) {
+            self.block.frame.origin = CGPoint(x: self.view.center.x - (self.block.frame.width / 2), y: 0)
+        } completion: { (success) in
+            self.rightMove()
+        }
+    }
+    
+    func rightMove() {
+        UIView.animate(withDuration: 1) {
+            self.block.frame.origin = CGPoint(x: self.view.frame.size.width - self.block.frame.width, y: self.view.center.y - (self.block.frame.height / 2))
+        } completion: { (success) in
+            self.downMove()
+        }
+    }
+    
+    func downMove() {
+        UIView.animate(withDuration: 1) {
+            self.block.frame.origin = CGPoint(x: self.view.center.x - (self.block.frame.width / 2), y: self.view.frame.size.height - self.block.frame.height)
+        } completion: { (success) in
+            self.leftMove()
+        }
+    }
+    
+    func leftMove() {
+        UIView.animate(withDuration: 1) {
+            self.block.frame.origin = CGPoint(x: 0, y: self.view.center.y - (self.block.frame.height / 2))
+        } completion: { (success) in
+            self.resetMove()
+        }
+    }
+    
+    func resetMove() {
+        
+        UIView.animate(withDuration: 1) {
+            self.block.center = CGPoint(x: self.view.center.x, y: self.view.center.y)
+        }
+    }
+    
 }
 
